d5a255cc0198202b0f674697c5792dd6e8a3a84f
ttrkaya <tupcuhamdi@hotmail.com>
2017-07-22 20:40:34 +0200

    #0001

---
+++ HappyPhysics/Assets/Main.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public GameObject prefabRect;
+    List<GameObject> rects = new List<GameObject>();
+    int rectsAt;
+    GameObject GetPooledRect() {
+        if(rectsAt == rects.Count) {
+            GameObject newRect = Instantiate(prefabRect);
+            rects.Add(newRect);
+        }
+        return rects[rectsAt++];
+    }
+
+
+	void Start () {
+		
+	}
+
+    float xx = 0;
+	void Update () {
+        rectsAt = 0;
+        xx += Time.deltaTime * 0.1f;
+        DrawRect(xx, 0.25f, 0.3f, 0.3f);
+        DrawRect(-0.25f, -0.25f, 0.1f, 0.1f);
+
+        // destroy unused rects
+        while(rects.Count > rectsAt) {
+            Destroy(rects[rects.Count - 1]);
+            rects.RemoveAt(rects.Count - 1);
+        }
+    }
+
+    void DrawRect(float centerX, float centerY, float widht, float height) {
+        var rect = GetPooledRect();
+        rect.transform.position = new Vector3(centerX, centerY);
+        rect.transform.localScale = new Vector3(widht, height, 1f);
+    }
+}
