61d8795cbcf58f551eee9213dc67a9c40d4bddd7
ttrkaya <tupcuhamdi@hotmail.com>
2017-08-13 16:05:15 +0200

    #0011;

--- HappyPhysics/Assets/Main.cs
+++ HappyPhysics/Assets/Main.cs
@@ -27,8 +27,10 @@ public struct V2 {
     public static V2 operator /(V2 v, float s) { return new V2 { x = v.x / s, y = v.y / s }; }
     public static V2 operator /(float s, V2 v) { return v / s; }
 
-    public float len2() { return x * x + y * y; }
-    public float len() { return Math.Sqrt(len2()); }
+    public static float operator *(V2 a, V2 b) { return a.x * b.x + a.y * b.y; } // dot product
+
+    public float Len2() { return x * x + y * y; }
+    public float Len() { return Math.Sqrt(Len2()); }
 }
 
 public class Body {
@@ -66,18 +68,25 @@ public class Main : MonoBehaviour {
     }
 
     List<Body> bodies = new List<Body> {
-        new Body {
-            center = new V2 { x = 0f, y = 0f},
-            vel = new V2(),
-            r = 0.1f,
-            invMass = 1f,
-        },
+        
     };
 
     void Start () {
+        bodies.Add(new Body {
+            center = new V2 { x = -1f, y = 0f },
+            vel = new V2 { x = 1f, y = 0f },
+            r = 0.1f,
+            invMass = 1f,
+        });
+        bodies.Add(new Body {
+            center = new V2 { x = 1f, y = 0f },
+            vel = new V2 { x = -1f, y = 0f },
+            r = 0.1f,
+            invMass = 1f,
+        });
+
         foreach(var i in bodies) {
-            i.vel.x = Random.Range(-1f, 1f);
-            i.vel.y = Random.Range(-1f, 1f);
+            i.vel.y = Random.Range(-0.1f, 0.1f);
         }
 	}
     
@@ -91,53 +100,46 @@ public class Main : MonoBehaviour {
             i.center += i.vel * dt;
         }
         
-        //int n = bodies.Count;
-        //for(int i = n - 1; i >= 0; i--) {
-        //    var a = bodies[i];
-        //    for(int j = i - 1; j >= 0; j--) {
-        //        var b = bodies[j];
-        //
-        //        float totSize = a.size + b.size;
-        //        float dp = b.center - a.center;
-        //        float dpAbs = Mathf.Abs(dp);
-        //        bool collided = dpAbs <= (totSize * 0.5f);
-        //        if(collided) {
-        //            float dv = b.vel - a.vel;
-        //            bool movingTowardsEachOther = dp * dv < 0;
-        //            if(movingTowardsEachOther) {
-        //                float totInvMass = a.invMass + b.invMass;
-        //                float invMassRatioA = a.invMass / totInvMass;
-        //                float invMassRatioB = b.invMass / totInvMass;
-        //
-        //                // eliminate overlapping (separation)
-        //                float totOverlap = (totSize * 0.5f) - dpAbs;
-        //                float dpSign = dp / dpAbs;
-        //                b.center += totOverlap * invMassRatioB * dpSign;
-        //                a.center -= totOverlap * invMassRatioA * dpSign;
-        //
-        //                // bounce
-        //                const float BOUNCINESS = 0.99f; // [0, 1]
-        //                float desiredDv = -BOUNCINESS * dv;
-        //                float desiredDvChange = desiredDv - dv;
-        //                
-        //                float dva = 1 * a.invMass;
-        //                float dvb = -1 * b.invMass;
-        //                float dvChange = dvb - dva;
-        //                float imp = desiredDvChange / dvChange;
-        //                a.ApplyImpulse(imp);
-        //                b.ApplyImpulse(-imp);
-        //
-        //                // algebraic method
-        //                //float va = a.vel;
-        //                //float vb = b.vel;
-        //                //a.vel = -a.invMass * va + 2f * a.invMass * vb + b.invMass * va;
-        //                //a.vel /= totInvMass;
-        //                //b.vel = -b.invMass * vb + 2f * b.invMass * va + a.invMass * vb;
-        //                //b.vel /= totInvMass;
-        //            }
-        //        }
-        //    }
-        //}
+        int n = bodies.Count;
+        for(int i = n - 1; i >= 0; i--) {
+            var a = bodies[i];
+            for(int j = i - 1; j >= 0; j--) {
+                var b = bodies[j];
+        
+                float tr = a.r + b.r;
+                V2 dp = b.center - a.center;
+                bool collided = dp.Len2() <= tr * tr;
+                if(collided) {
+                    V2 dv = b.vel - a.vel;
+                    bool movingTowardsEachOther = dp * dv < 0;
+                    if(movingTowardsEachOther) {
+                        bodies.Clear();
+                        return;
+                        //float totInvMass = a.invMass + b.invMass;
+                        //float invMassRatioA = a.invMass / totInvMass;
+                        //float invMassRatioB = b.invMass / totInvMass;
+                        //
+                        //// eliminate overlapping (separation)
+                        //float totOverlap = (totSize * 0.5f) - dpAbs;
+                        //float dpSign = dp / dpAbs;
+                        //b.center += totOverlap * invMassRatioB * dpSign;
+                        //a.center -= totOverlap * invMassRatioA * dpSign;
+                        //
+                        //// bounce
+                        //const float BOUNCINESS = 0.99f; // [0, 1]
+                        //float desiredDv = -BOUNCINESS * dv;
+                        //float desiredDvChange = desiredDv - dv;
+                        //
+                        //float dva = 1 * a.invMass;
+                        //float dvb = -1 * b.invMass;
+                        //float dvChange = dvb - dva;
+                        //float imp = desiredDvChange / dvChange;
+                        //a.ApplyImpulse(imp);
+                        //b.ApplyImpulse(-imp);
+                    }
+                }
+            }
+        }
 
         // ------ Rendering ----------
         rectsAt = 0;
@@ -147,7 +149,7 @@ public class Main : MonoBehaviour {
             DrawCircle(i.center, i.r);
         }
         
-        // destroy unused rects
+        // destroy unused
         while(rects.Count > rectsAt) {
             Destroy(rects[rects.Count - 1]);
             rects.RemoveAt(rects.Count - 1);
